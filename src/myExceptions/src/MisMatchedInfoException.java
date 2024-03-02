580b6897950679e12c1d14e0113f609ddc305095
Shuteng <117995375+Shuteng-0608@users.noreply.github.com>
2024-03-02 15:09:12 +0000

    Update MisMatchedInfoException.java

--- src/myExceptions/src/MisMatchedInfoException.java
+++ src/myExceptions/src/MisMatchedInfoException.java
@@ -1,3 +1,7 @@
+// should decide what makes valid data
+// (e.g., length, range, number of characters, etc.) If an error is found, just continue without
+// that line of data. Provide suitable data to check that your program is working correctly, e.g.
+// input files with some errors.
 public class MisMatchedInfoException extends Exception{
     public MisMatchedInfoException() {
         super("Mismatched passenger information ask the passenger to enter again to Check-in");
