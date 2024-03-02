b9855bc176b86d96a177c232ad483074b5c1cb98
Shuteng <117995375+Shuteng-0608@users.noreply.github.com>
2024-03-02 15:10:24 +0000

    Update MissingInputAttributeException.java

--- src/myExceptions/src/MissingInputAttributeException.java
+++ src/myExceptions/src/MissingInputAttributeException.java
@@ -1,3 +1,9 @@
+/*
+ * should decide what makes valid data
+ * (e.g., length, range, number of characters, etc.) If an error is found, just continue without
+ * that line of data. Provide suitable data to check that your program is working correctly, e.g.
+ * input files with some errors.
+ */
 public class MissingInputAttributeException extends Exception{
     public MissingInputAttributeException() {
         super("Invalid input data found, continue without this line of data");
