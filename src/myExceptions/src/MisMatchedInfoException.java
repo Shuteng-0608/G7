fd28f10c8272d5eb61ab6afa3730fa5af722ba5f
Shuteng <117995375+Shuteng-0608@users.noreply.github.com>
2024-03-02 15:15:20 +0000

    Update MisMatchedInfoException.java

--- src/myExceptions/src/MisMatchedInfoException.java
+++ src/myExceptions/src/MisMatchedInfoException.java
@@ -1,3 +1,7 @@
+/*
+ * Retrieve the passenger information does not match
+ * Ask the passenger to check personal information and then re-Checkin
+ */
 public class MisMatchedInfoException extends Exception{
     public MisMatchedInfoException() {
         super("Mismatched passenger information ask the passenger to enter again to Check-in");
