de36705c9b6ad43f19d7fd8492d43021b77edf79
1125115069 <72552663+1125115069@users.noreply.github.com>
2024-03-04 17:32:47 +0000

    Update FlightList.java

--- src/checkinSys/FlightList.java
+++ src/checkinSys/FlightList.java
@@ -4,48 +4,47 @@ import java.util.ArrayList;
 
 public class FlightList {
 	// Storage for an arbitrary number of details.
-	private ArrayList <Flight> flightList;
-	
-    /**
-     * Perform any initialization for the text file.
-     */
-    public FlightList()
-    {
-    	flightList = new ArrayList<Flight>() ;
-    }
-    
-    /**
-     * Look up a flight code and return the
-     * corresponding flight details.
-     * @param flight_code The flight code to be looked up.
-     * @return The details corresponding to the flight code, null if none
-     */
-    public Flight findByCode(String flight_code)
-    {
-    	for (Flight f : flightList)
-    	{
-    		if (f.getFlight().equals(flight_code))
-    		{
-    			return f;
-    		}
-    	}
-    	return null;
-    }
-    
-    /**
-     * Add a new set of details to the list
-     * @param flight The details of the flight
-     */
-    public void addFlight(Flight flight) 
-    {
-    	flightList.add(flight);
-    }
-    
-    /**
-     * @return The number of entries currently in the flightList.
-     */
-    public int getNumberOfEntries()
-    {
-        return flightList.size();
-    }
+	private ArrayList<Flight> flightList;
+
+	/**
+	 * Perform any initialization for the text file.
+	 */
+	public FlightList() {
+		flightList = new ArrayList<Flight>();
+	}
+
+	/**
+	 * Look up a flight code and return the corresponding flight details.
+	 * 
+	 * @param flight_code The flight code to be looked up.
+	 * @return The details corresponding to the flight code, null if none
+	 */
+	public Flight findByCode(String flight_code) {
+		for (Flight f : flightList) {
+			if (f.getFlight().equals(flight_code)) {
+				return f;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Add a new set of details to the list
+	 * 
+	 * @param flight The details of the flight
+	 */
+	public void addFlight(Flight flight) {
+		flightList.add(flight);
+	}
+
+	public Flight getFlight(int idx) {
+		return flightList.get(idx);
+	}
+
+	/**
+	 * @return The number of entries currently in the flightList.
+	 */
+	public int getNumberOfEntries() {
+		return flightList.size();
+	}
 }
