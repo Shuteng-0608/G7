ce36cd0515f83701c3f44351d888fe56aeb8a4d4
Shuteng <117995375+Shuteng-0608@users.noreply.github.com>
2024-03-06 13:57:50 +0000

    Delete src/myExceptions/IncorrectBookRefException.java

--- src/myExceptions/IncorrectBookRefException.java
+++
@@ -1,8 +0,0 @@
-package myExceptions;
-
-public class IncorrectBookRefException extends Exception{
-
-    public IncorrectBookRefException() {
-        super("The generated Booking-Reference is INCORRECT! Check the rule of Booking-Ref Generation!");
-    }
-}
