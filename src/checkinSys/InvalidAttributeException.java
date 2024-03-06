732d1aed0c0c3be36992869760ff8cb976d370f9
Shuteng <117995375+Shuteng-0608@users.noreply.github.com>
2024-03-06 10:07:38 +0000

    Add files via upload

---
+++ src/checkinSys/InvalidAttributeException.java
@@ -0,0 +1,14 @@
+package flightSystem;
+
+/*
+ * Should decide what makes valid data (e.g., length, range, number of characters, etc.)
+ * If an error is found, just continue without that line of data.
+ * Provide suitable data to check that your program is working correctlye.g.
+ * e.g. input files with some errors.
+ */
+public class InvalidAttributeException extends Exception{
+    public InvalidAttributeException(String message) {
+        super(message);
+    }
+}
+
