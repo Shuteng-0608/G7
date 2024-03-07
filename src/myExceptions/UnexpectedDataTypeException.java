1fa79b3805e540d508d27d1bbeda07836a36ce1e
Shuteng <117995375+Shuteng-0608@users.noreply.github.com>
2024-03-07 15:06:36 +0000

    Delete src/myExceptions/UnexpectedDataTypeException.java

--- src/myExceptions/UnexpectedDataTypeException.java
+++
@@ -1,9 +0,0 @@
-package myExceptions;
-/*
- * Illegal type of data entered by the passenger
- */
-public class UnexpectedDataTypeException extends Exception{
-    public UnexpectedDataTypeException() {
-        super("Unexpected DataType! Ask the passenger to enter their information again");
-    }
-}
