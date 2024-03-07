098f2f739683fd41704f773d6a50a24ca893c990
Leijunjie-nuclear <72396323+Leijunjie-nuclear@users.noreply.github.com>
2024-03-07 14:11:56 +0000

    Update ManagerTest.java

--- src/test/ManagerTest.java
+++ src/test/ManagerTest.java
@@ -12,44 +12,29 @@ class ManagerTest {
 	
 	Manager manager;
 	@BeforeEach
-	void testPassenger() throws InvalidAttributeException {
+	void testManager() throws InvalidAttributeException {
 		manager = new Manager();
 	}
 	
 	@Test
 	void testFindPassenger() {
-		Passenger p = manager.findPassenger("Wright", "JFKCA3191811246621");
-		assertEquals("Crystal Wright", p.getName(), "Equal!");
+		Passenger p = manager.findPassenger("Brewer", "DXBCA3781807232238");
+		assertEquals("Diane Brewer", p.getName(), "Equal!");
 		assertNotEquals("Stephanie Gomez", p.getName(), "Error!");
 	}
 
 	@Test
 	void testExcess_fee() {
-		double fee = manager.excess_fee("Wright", "JFKCA3191811246621", 22, 3);
-//		assertEquals(0, p.getName(), "Equal!");
-		assertNotEquals(0, fee, "Error!");
+		double fee = manager.excess_fee("Brewer", "DXBCA3781807232238", 26.42, 1.1);
+		assertEquals(19.26, fee, "Equal!");
+		assertNotEquals(19.12, fee, "Error!");
 	}
-
-//	@Test
-//	void testReport() {
-//		fail("Not yet implemented");
-//	}
 
 	@Test
 	void testFindFlight() {
-		Flight f = manager.findFlight("BA524");
-		assertEquals("BA524", f.getFlight(), "Equal!");
+		Flight f = manager.findFlight("EK660");
+		assertEquals("EK660", f.getFlight(), "Equal!");
 		assertNotEquals("LH887", f.getFlight(), "Error!");
 	}
-
-//	@Test
-//	void testValidateFlightData() {
-//		fail("Not yet implemented");
-//	}
-
-//	@Test
-//	void testValidatePassengerData() {
-//		fail("Not yet implemented");
-//	}
-
+	
 }
