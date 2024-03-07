c4dd441797df80108ce7afc681dff71ee06af0ca
Leijunjie-nuclear <72396323+Leijunjie-nuclear@users.noreply.github.com>
2024-03-07 12:36:35 +0000

    Update FlightList.java

--- src/checkinSys/FlightList.java
+++ src/checkinSys/FlightList.java
@@ -1,44 +1,34 @@
 package checkinSys;
 
-//maintains a list of Flight objects as an ArrayList
-
+/* Maintains a list of Flight objects as an ArrayList. */
 import java.util.ArrayList;
 
 public class FlightList {
-	// Storage for an arbitrary number of details.
+	// Storage for an arbitrary number of flights.
 	private ArrayList<Flight> flightList;
 
 	/**
-	 * Perform any initialization for the text file.
+	 * Perform any initialization for the flight list.
 	 */
 	public FlightList() {
 		flightList = new ArrayList<Flight>();
 	}
 
 	/**
-	 * Look up a flight code and return the corresponding flight details.
-	 * 
-	 * @param flight_code The flight code to be looked up.
-	 * @return The details corresponding to the flight code, null if none
-	 */
-	public Flight findByCode(String flight_code) {
-		for (Flight f : flightList) {
-			if (f.getFlight().equals(flight_code)) {
-				return f;
-			}
-		}
-		return null;
-	}
-
-	/**
 	 * Add a new set of details to the list
 	 * 
-	 * @param flight The details of the flight
+	 * @param flight The object of the flight
 	 */
 	public void addFlight(Flight flight) {
 		flightList.add(flight);
 	}
 
+	/**
+	 * Look up an index and return the corresponding flight object.
+	 * 
+	 * @param idx The index to be looked up.
+	 * @return The flight object corresponding to the index.
+	 */
 	public Flight getFlight(int idx) {
 		return flightList.get(idx);
 	}
@@ -48,5 +38,20 @@ public class FlightList {
 	 */
 	public int getNumberOfEntries() {
 		return flightList.size();
+	}
+
+	/**
+	 * Look up a flight code and return the corresponding flight object.
+	 * 
+	 * @param flight_code The flight code to be looked up.
+	 * @return The object corresponding to the flight code, null if none.
+	 */
+	public Flight findByCode(String flight_code) {
+		for (Flight f : flightList) {
+			if (f.getFlight().equals(flight_code)) {
+				return f;
+			}
+		}
+		return null;
 	}
 }
