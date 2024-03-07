e7f6d556c8377f37e12b7dbd183b7bdd189dd170
Shuteng <117995375+Shuteng-0608@users.noreply.github.com>
2024-03-07 15:06:23 +0000

    Delete src/myExceptions/UnexpectedDataRangeException.java

--- src/myExceptions/UnexpectedDataRangeException.java
+++
@@ -1,9 +0,0 @@
-package myExceptions;
-/*
- * Illegal range of data entered by the passenger
- */
-public class UnexpectedDataRangeException extends Exception{
-    public UnexpectedDataRangeException() {
-        super("Unexpected data range! Ask the passenger enter their information again");
-    }
-}
