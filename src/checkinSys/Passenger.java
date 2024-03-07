7ce811837c2dc572594882c25bb75b9c8c759349
Leijunjie-nuclear <72396323+Leijunjie-nuclear@users.noreply.github.com>
2024-03-07 12:36:22 +0000

    Update Passenger.java

--- src/checkinSys/Passenger.java
+++ src/checkinSys/Passenger.java
@@ -1,44 +1,33 @@
 package checkinSys;
-// a simple class to contain and manage Passengers details
+/* A simple class to contain and manage Passengers details. */
 
-import java.io.IOException;
-import java.util.HashMap;
-import myExceptions.*;
-
-/**
- * A class to manage Passenger details in a check-in system.
- * It implements the Comparable interface for sorting passengers based on reference code.
- */
 public class Passenger implements Comparable<Passenger> {
-	// Fields for passenger details
 	private String reference_code;
 	private String name;
 	private String flight_code;
+	private String date;
 	private String check_in;
 	private double weight;
 	private double volume;
 
 	/**
-	 * Constructor to initialize a passenger with all details.
-	 * @param reference_code The reference code of the passenger.
-	 * @param name           The name of the passenger.
-	 * @param flight_code    The code of the flight the passenger is booked on.
-	 * @param check_in       The check-in status of the passenger.
-	 * @param weight         The weight of the passenger's baggage.
-	 * @param volume         The volume of the passenger's baggage.
+	 * Set up the passengers details.
 	 */
-	public Passenger(String reference_code, String name, String flight_code, String check_in, double weight, double volume) {
+	public Passenger(String reference_code, String name, String flight_code, String date, String check_in,
+			double weight, double volume) {
 		this.reference_code = reference_code.trim();
 		this.name = name.trim();
 		this.flight_code = flight_code.trim();
-		this.check_in = check_in;
+		this.date = date.trim();
+		this.check_in = check_in.trim();
 		this.weight = weight;
 		this.volume = volume;
-
 	}
-	// Default constructor
-	public Passenger() {}
-
+	
+	public Passenger() {
+		
+	}
+	
 	/**
 	 * @return The reference code.
 	 */
@@ -61,7 +50,14 @@ public class Passenger implements Comparable<Passenger> {
 	}
 
 	/**
-	 * @return The check_in status.
+	 * @return The date
+	 */
+	public String getDate() {
+		return date;
+	}
+
+	/**
+	 * @return The check-in status.
 	 */
 	public String getCheckin() {
 		return check_in;
@@ -81,8 +77,16 @@ public class Passenger implements Comparable<Passenger> {
 		return volume;
 	}
 
-	public void check_in() {
-		check_in = "Yes";
+	/**
+	 * @return true if checked in, false otherwise.
+	 */
+	public boolean check_in() {
+		if (check_in.equals("Yes")) {
+			return true;
+		} else {
+			check_in = "Yes";
+			return false;
+		}
 	}
 
 	public void set(double weight, double volume) {
@@ -110,8 +114,8 @@ public class Passenger implements Comparable<Passenger> {
 	 */
 	public boolean equals(Object other) {
 		if (other instanceof Passenger) {
-			Passenger otherStaff = (Passenger) other;
-			return reference_code.equals(otherStaff.getReference());
+			Passenger otherPassenger = (Passenger) other;
+			return reference_code.equals(otherPassenger.getReference());
 		} else {
 			return false;
 		}
@@ -135,11 +139,8 @@ public class Passenger implements Comparable<Passenger> {
 	 * @return A string containing all details.
 	 */
 	public String toString() {
-		return String.format("%-8s", reference_code)
-				+ String.format("%-20s", name)
-				+ String.format("%-8s", flight_code)
-				+ String.format("%-3s", check_in);
+		return String.format("%-20s", reference_code) + String.format("%-20s", name)
+				+ String.format("%-8s", flight_code) + String.format("%-8s", date) + String.format("%-3s", check_in)
+				+ String.format("%f", weight) + String.format("%f", volume);
 	}
-	
-	
 }
