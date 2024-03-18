f08db4e2aad556b9ef1aaf48f3c95dee1434caa2
Leijunjie-nuclear <72396323+Leijunjie-nuclear@users.noreply.github.com>
2024-03-18 16:46:24 +0000

    Update flight.java

--- src/models/flight.java
+++ src/models/flight.java
@@ -10,6 +10,7 @@ public class flight {
     // Flight details
     private String flightCode;
     private String destination;
+    private String date;
     private int capacity;
     private String carrier;
     private int checkedInPassengers;
@@ -18,12 +19,17 @@ public class flight {
     private double volume;
     private double maxWeight;
     private double maxVolume;
-
+    
     // Constructor
-    public flight(String flightCode, String destination, int capacity,
+    
+    public flight(){}
+    
+    public flight(String flightCode, String date, String destination, String carrier, int capacity,
                   double maxWeight, double maxVolume) {
         this.flightCode = flightCode;
+        this.date = date;
         this.destination = destination;
+        this.carrier = carrier;
         this.capacity = capacity;
         this.maxWeight = maxWeight;
         this.maxVolume = maxVolume;
@@ -52,6 +58,114 @@ public class flight {
         return false;
     }
     
+	/**
+	 * @return The number of passengers checked-in.
+	 */
+	public int numberOfCheckIn() {
+		int cnt = 0;
+		for (int i = 0; i < passengerList.getNumberOfEntries(); i++) {
+			passenger p = passengerList.getByIdx(i);
+			if (p.getCheckin().equals("Yes"))
+				cnt++;
+		}
+		return cnt;
+	}
+
+	/**
+	 * @return The total weight of the baggage.
+	 */
+	public double totalWeight() {
+		double sum = 0;
+		for (int i = 0; i < passengerList.getNumberOfEntries(); i++) {
+			passenger p = passengerList.getByIdx(i);
+			if (p.getCheckin().equals("Yes"))
+				sum += p.getBaggageWeight();
+		}
+		return sum;
+	}
+
+	/**
+	 * @return The total volume of the baggage.
+	 */
+	public double totalVolume() {
+		double sum = 0;
+		for (int i = 0; i < passengerList.getNumberOfEntries(); i++) {
+			passenger p = passengerList.getByIdx(i);
+			if ((p.getCheckin().equals("Yes")))
+				sum += p.getBaggageVolume();
+		}
+		return sum;
+	}
+
+	/**
+	 * @return The total excess baggage fees.
+	 */
+	public double totalFees() {
+		double sum = 0;
+		for (int i = 0; i < passengerList.getNumberOfEntries(); i++) {
+			passenger p = passengerList.getByIdx(i);
+			if ((p.getCheckin().equals("Yes")))
+				sum += p.excess_fee();
+		}
+		return sum;
+	}
+
+	/**
+	 * Determine if the flight is overweight.
+	 */
+	public boolean check_weight() {
+		if (totalWeight() > maxWeight)
+			return false;
+		return true;
+	}
+
+	/**
+	 * Determine if the flight exceeds its maximum volume.
+	 */
+	public boolean check_volume() {
+		if (totalVolume() > maxVolume)
+			return false;
+		return true;
+	}
+
+	/**
+	 * Test for content equality between two flights.
+	 * 
+	 * @param other The object to compare to this one.
+	 * @return true if the argument object has the same flight code.
+	 */
+	public boolean equals(Object other) {
+		if (other instanceof flight) {
+			flight otherFlight = (flight) other;
+			return flightCode.equals(otherFlight.getFlightCode());
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Compare this Flight object against another, for the purpose of sorting. The
+	 * fields are compared by flight code.
+	 * 
+	 * @param otherFlight The flight to be compared against.
+	 * @return a negative integer if this flight_code comes before the parameter's
+	 *         flight_code, zero if they are equal and a positive integer if this
+	 *         comes after the other.
+	 */
+
+	public int compareTo(flight otherFlight) {
+		return flightCode.compareTo(otherFlight.getFlightCode());
+	}
+
+	/**
+	 * @return A string containing all details.
+	 */
+	public String toString() {
+		return String.format("%-8s", flightCode) + String.format("%-8s", date) + String.format("%-5s", destination)
+				+ String.format("%-5s", carrier) + String.format("%d", capacity) + String.format("%f", maxWeight)
+				+ String.format("%f", maxVolume);
+	}
+    
     
 	/**
      * Calculates the baggage volume percentage used of the flight.
@@ -60,7 +174,7 @@ public class flight {
     public double calculateBaggageVolumePercentage() {
         return (volume / maxVolume) * 100;
     }
-
+    
     /**
      * Calculates the baggage weight percentage used of the flight.
      * @return The percentage of baggage weight used.
