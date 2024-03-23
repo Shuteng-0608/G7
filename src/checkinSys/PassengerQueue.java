0287152382906d2096cde04e11e17036c1168c20
1125115069 <72552663+1125115069@users.noreply.github.com>
2024-03-23 15:47:29 +0000

    Update PassengerQueue.java

--- src/checkinSys/PassengerQueue.java
+++ src/checkinSys/PassengerQueue.java
@@ -1,41 +1,34 @@
-package checkInSys;
-
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class PassengerQueue implements Runnable {
 	
 	private final String queueType;
-    private final List<Passenger> queue;
-    private final List<Passenger> list;
+	private SharedObject so;
     private boolean isOpen;
     
-	public PassengerQueue(String queueType, List<Passenger> queue, List<Passenger> list) {
+	public PassengerQueue(String queueType, SharedObject so) {
 		this.queueType = queueType;
-		this.queue = queue;
-		this.list = list;
+		this.so = so;
 		this.isOpen = true;
 	}
 	
-	public void closeQueue() {
+	public void queueClose() {
         isOpen = false;
     }
 	
 	@Override
     public void run() {
-        while (isOpen) {
-            if (!list.isEmpty()) {
-                Passenger nextPassenger = list.remove(0);
-                queue.add(nextPassenger);
-                Logger.log(queueType + " adding passenger: " + nextPassenger.getName());
-                // Simulate processing time
-                try {
-                    Thread.sleep(1000); // Simulate processing time
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                Logger.log(queueType + " finished adding passenger: " + nextPassenger.getName());
-            }
-        }
+		while(isOpen) {
+			Passenger p = so.getFlightList().randomSelect();
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			so.addQueue(p);
+		}
     }
 
 }
