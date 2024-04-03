dcac078fab20c384e5d3dace285ad19237a651a2
1125115069 <72552663+1125115069@users.noreply.github.com>
2024-04-03 23:13:12 +0100

    Update Controller.java

--- src/checkinSys/Controller.java
+++ src/checkinSys/Controller.java
@@ -13,78 +13,50 @@ import javax.swing.JSlider;
 import GUI.AirportGUI;
 
 public class Controller {
-	
-	
 	public Controller(AirportGUI airport, SharedObject so) {
 		airport.setSharedObject(so);
 		startSpeedSlider(airport);
-		startCheckInDeskBox(airport);
 		new Thread(airport).start();
+		for(CheckInDesk desk: airport.getDesk())
+			startCheckInDeskBox(desk);
 	}
-	
-	
-	public void startCheckInDeskBox(AirportGUI airport) {
-		for (int i = 1; i <= 5; i++) {
-            final int deskNumber = i; // ʹ��final�����Ա�����������ʹ��
-           
 
-			airport.getJBoxs()[i].addActionListener(new ActionListener() {
-		          @Override
-		          public void actionPerformed(ActionEvent e) {
-		              JCheckBox cb = (JCheckBox) e.getSource();
-//		              System.out.println(airport.getDeskNum());
-		              if (cb.isSelected()) {
-		            	  airport.getJDeskButs()[deskNumber].setBorder(BorderFactory.createLineBorder(Color.BLACK)); // δ��ѡʱ�߿��غ�ɫ
-		              	
-			              if (airport.getDeskNum() == 1 || airport.getDeskNum() == 2 || airport.getDeskNum() == 3) {
-			            	  cb.setText("Business Desk " + airport.getDeskNum() + " Close"); // ���¸�ѡ���ı�Ϊ��ʼ״̬
-		            		  Logger.log("Business Desk " + airport.getDeskNum() + " Close");
-
-			              } else {
-			            	  cb.setText("Economy Desk " + airport.getDeskNum() + " Close"); // ���¸�ѡ���ı�Ϊ��ʼ״̬
-		            		  Logger.log("Economy Desk " + airport.getDeskNum() + " Close");
-
-			              }
-		              	
-		                  
-			              for (CheckInDesk desk_ : airport.getDesk()) {
-			            	  if (desk_.getDeskName().equals("Desk " + airport.getDeskNum())) {
-			            		  System.out.println("Desk " + airport.getDeskNum() + " Close");
-			            		  Logger.log("Desk " + airport.getDeskNum() + " Close");
-			            		  desk_.closeDesk();
-			            	  }
-			              }
-		              } else {
-
-		            	  airport.getJDeskButs()[deskNumber].setBorder(BorderFactory.createLineBorder(Color.GREEN)); // ��ѡʱ�߿����
-		                  cb.setText("Desk " + airport.getDeskNum() + " Open"); // ���¸�ѡ���ı�ΪOpen
-	            		  Logger.log("Desk " + airport.getDeskNum() + " Open");
-		              }
-		          }
-		      });
+	public void startCheckInDeskBox(CheckInDesk desk) {
+			desk.getButton().addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					JCheckBox jcb = (JCheckBox) e.getSource(); // 得到产生的事件
+					if (jcb.isSelected()) {
+						desk.getButtonPanel().setBorder(BorderFactory.createLineBorder(Color.RED)); 
+						jcb.setText(desk.getDeskType()+ ' ' + desk.getDeskName() + " Close"); 
+						desk.getText().setText("");
+						Logger.log(desk.getDeskType()+ ' ' + desk.getDeskName() + " Close"); 
+						desk.closeDesk();
+					}
+					else {
+						desk.getButtonPanel().setBorder(BorderFactory.createLineBorder(Color.GREEN)); 
+						jcb.setText(desk.getDeskType()+ ' ' + desk.getDeskName() + " Open"); 
+						Logger.log(desk.getDeskType()+ ' ' + desk.getDeskName() + " Open"); 
+						desk.restartDesk();
+					}
+				}
+			});
 		}
-		
-		
-	}
-	
-	
+
 	public void startSpeedSlider(AirportGUI airport) {
 		airport.getSpeedSlider().addChangeListener(e -> {
-			JSlider source = (JSlider)e.getSource();
+			JSlider source = (JSlider) e.getSource();
 			if (!source.getValueIsAdjusting()) {
-				airport.setTimerSpeed(1000000/source.getValue());
-         
+				airport.setTimerSpeed(1000000 / source.getValue());
+
 				// Adjust all timers according to the new speed
 				airport.adjustTimerSpeeds();
 			}
 		});
 	}
-	
-	
-	
-	
+
 	public static void main(String[] args) {
-		
+
 		SharedObject so = new SharedObject();
 		AirportGUI airport = new AirportGUI(so);
 		Controller c = new Controller(airport, so);
