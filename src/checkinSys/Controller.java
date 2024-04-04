dae1f30865daf3e3cbcfe8c0dcf36403919d62d3
1125115069 <72552663+1125115069@users.noreply.github.com>
2024-04-04 03:06:08 +0100

    Update Controller.java

--- src/checkinSys/Controller.java
+++ src/checkinSys/Controller.java
@@ -1,4 +1,4 @@
-package checkInSimulation;
+package checkinSys;
 
 import java.awt.Color;
 import java.awt.Dimension;
@@ -11,36 +11,31 @@ import javax.swing.JPanel;
 import javax.swing.JSlider;
 
 import GUI.AirportGUI;
-// Controller class for handling the interaction between the GUI and the simulation logic
+
 public class Controller {
-	// Constructor initializes the controller with the airport GUI and the shared object
-	public Controller(AirportGUI airport, SharedObject so) {
-		airport.setSharedObject(so);// Sets the shared object used for synchronization
-		startSpeedSlider(airport);// Initializes the slider to adjust simulation speed
-		new Thread(airport).start();// Starts the GUI in a separate thread
-		// Initializes each check-in desk with a toggle functionality
-		for(CheckInDesk desk: airport.getDesk())
+	public Controller(AirportGUI airport) {
+		startSpeedSlider(airport);
+		for(CheckInDesk desk: airport.getDesks())
 			startCheckInDeskBox(desk);
+		new Thread(airport).start();
 	}
 
 	public void startCheckInDeskBox(CheckInDesk desk) {
 			desk.getButton().addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					JCheckBox jcb = (JCheckBox) e.getSource(); // µÃµ½²úÉúµÄÊÂ¼þ
+					JCheckBox jcb = (JCheckBox) e.getSource(); // 得到产生的事件
 					if (jcb.isSelected()) {
 						desk.getButtonPanel().setBorder(BorderFactory.createLineBorder(Color.RED)); 
 						jcb.setText(desk.getDeskType()+ ' ' + desk.getDeskName() + " Close"); 
-						jcb.setForeground(Color.red);
 						desk.getText().setText("");
-						Logger.log(Logger.LogLevel.INFO, desk.getDeskType()+ ' ' + desk.getDeskName() + " Close"); 
+						Logger.log(desk.getDeskType()+ ' ' + desk.getDeskName() + " Close"); 
 						desk.closeDesk();
 					}
 					else {
 						desk.getButtonPanel().setBorder(BorderFactory.createLineBorder(Color.GREEN)); 
-						jcb.setText(desk.getDeskType()+ ' ' + desk.getDeskName() + " Open");
-						jcb.setForeground(Color.black);
-						Logger.log(Logger.LogLevel.INFO, desk.getDeskType()+ ' ' + desk.getDeskName() + " Open"); 
+						jcb.setText(desk.getDeskType()+ ' ' + desk.getDeskName() + " Open"); 
+						Logger.log(desk.getDeskType()+ ' ' + desk.getDeskName() + " Open"); 
 						desk.restartDesk();
 					}
 				}
@@ -60,11 +55,7 @@ public class Controller {
 	}
 
 	public static void main(String[] args) {
-		Logger logger = Logger.getInstance();
-
-		SharedObject so = new SharedObject();
-		AirportGUI airport = new AirportGUI(so);
-		Controller c = new Controller(airport, so);
+		AirportGUI airport = new AirportGUI();
+		Controller c = new Controller(airport);
 	}
-
 }
